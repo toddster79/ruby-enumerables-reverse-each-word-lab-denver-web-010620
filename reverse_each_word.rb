d82b8a138a3ed116b362243745a85eff9c98bb9f
Todd Carlson <todd.carlson3030@me.com>
2019-12-29 20:17:13 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,6 +1,5 @@
 def reverse_each_word(sentence)
-  array = []
-  array << sentence.reverse 
-  
-  array.each { |n| n }
+  reversed = (sentence.split.each do |word|
+    word.reverse
+  end).join(' ')
 end
